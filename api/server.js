590e6220541da9ad2ec78901bbfb0fa4ecae4abe
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-05-07 11:49:30 -0300

    feat: add Docker + docker-compose

--- api/server.js
+++ api/server.js
@@ -1,18 +1,21 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Corrija para 'db.json'
+const router = jsonServer.router("db.json"); // Corrija para 'db.json'
 const middlewares = jsonServer.defaults();
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  
+  res.header("Access-Control-Allow-Origin", "*"); // Permitir todas as origens
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+
   // Responder a solicitações OPTIONS
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
-  
+
   next();
 });
 
